a5c4ef77f73749b39614065e7e4730c184360921
amine-cheraitia <amine-cheraitia@hotmail.com>
2024-01-13 19:42:44 +0100

    adding helper methode

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 
 
-class HomeController
+class HomeController extends AbstractController
 {
 
 
     public function bonjour()
     {
         return new Response("hello");
+    }
+
+    public function aurevoir()
+    {
+        return $this->redirectToRoute('accueil');
+    }
+
+    public function redirectAilleur()
+    {
+        return $this->redirect('https://www.x.com');
     }
 }
