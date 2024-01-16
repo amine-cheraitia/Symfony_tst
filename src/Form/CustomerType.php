c95edcd23b2283b04bbacfdbde76c5b892f84f3e
amine-cheraitia <amine-cheraitia@hotmail.com>
2024-01-16 19:36:50 +0100

    ajout type du champ

--- src/Form/CustomerType.php
+++ src/Form/CustomerType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Customer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +15,9 @@ class CustomerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Nom')
-            ->add('email')
-        ;
+            ->add('Nom', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('Valider', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
