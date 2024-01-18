d4601a7c6b52ab00528f09aa1dc2290e43b6d00c
amine-cheraitia <amine-cheraitia@hotmail.com>
2024-01-19 00:07:24 +0100

    adding fromcustomer route with form validation

--- src/Controller/CostumerController.php
+++ src/Controller/CostumerController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Customer;
+use App\Form\CustomerType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CostumerController extends AbstractController
 {
-    #[Route('/costumer', name: 'app_costumer')]
-    public function index(): Response
+    #[Route('/formcostumer', name: 'formcostumer')]
+    public function index(Request $request): Response
     {
+        $customer = new Customer();
+        $customerForm = $this->createForm(CustomerType::class, $customer);
+
+        $customerForm->handleRequest($request);
+
+        if ($customerForm->isSubmitted() && $customerForm->isValid()) {
+        }
+
         return $this->render('costumer/index.html.twig', [
-            'controller_name' => 'CostumerController',
+            'customerform' => $customerForm->createView()
         ]);
     }
 }
